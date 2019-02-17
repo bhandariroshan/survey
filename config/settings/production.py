3581f568bd17eb53c7c6b98d663c3bfeaf2d205f
rbhandari <rbhandari@growbydata.com>
2019-02-17 23:14:45 +0545

    changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,20 +1,20 @@
 from .base import *  # noqa
 from .base import env
 
- # GENERAL
+# GENERAL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com'])
 
- # DATABASES
+# DATABASES
 # ------------------------------------------------------------------------------
 DATABASES['default'] = env.db('DATABASE_URL')  # noqa F405
 DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
 DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)  # noqa F405
 
- # CACHES
+# CACHES
 # ------------------------------------------------------------------------------
 CACHES = {
     'default': {
@@ -29,7 +29,7 @@ CACHES = {
     }
 }
 
- # SECURITY
+# SECURITY
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#secure-proxy-ssl-header
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
